d9880652119c15a3f549e831d017949521e6224b
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-01 17:25:18 -0600

    add migration for synonym_words join table

---
+++ db/migrate/20220301232237_create_join_table_synonym_word.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableSynonymWord < ActiveRecord::Migration[6.1]
+  def change
+    create_join_table :synonyms, :words do |t|
+      # t.index [:synonym_id, :word_id]
+      # t.index [:word_id, :synonym_id]
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_231527) do
+ActiveRecord::Schema.define(version: 2022_03_01_232237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,11 @@ ActiveRecord::Schema.define(version: 2022_03_01_231527) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "synonyms_words", id: false, force: :cascade do |t|
+    t.bigint "synonym_id", null: false
+    t.bigint "word_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
