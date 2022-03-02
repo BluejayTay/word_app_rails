9dd42573e33cfd886e90e64a1f4d6d25ae0c19bc
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-02 14:06:52 -0600

    Make user_id optional for study_list

--- app/models/study_list.rb
+++ app/models/study_list.rb
@@ -1,4 +1,5 @@
 class StudyList < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   has_and_belongs_to_many :words
+  validates :title, presence: true
 end
---
+++ db/migrate/20220302163627_change_column_null_user_study_list.rb
@@ -0,0 +1,5 @@
+class ChangeColumnNullUserStudyList < ActiveRecord::Migration[6.1]
+  def change
+    change_column :study_lists, :user_id, :bigint, null:true
+  end
+end
