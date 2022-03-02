ddeb4ce7024e759e70cf6627929547df6d6ce0cb
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-02 14:07:55 -0600

    create thesaurus_service

---
+++ app/services/thesaurus_service.rb
@@ -0,0 +1,6 @@
+class ThesaurusService
+  def self.look_up(word_query)
+    response = HTTParty.get("https://dictionaryapi.com/api/v3/references/thesaurus/json/#{word_query}?key=#{ENV['MW_thesaurus_api_key']}")
+    response["meta"]
+  end
+end
