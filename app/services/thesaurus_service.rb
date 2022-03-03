a68c3eba866e521029e4b964933993aa7d968db2
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-03 17:13:06 -0600

    Fixes code for ThesaurusService

--- app/services/thesaurus_service.rb
+++ app/services/thesaurus_service.rb
@@ -1,6 +1,5 @@
 class ThesaurusService
-  def self.look_up(word_query)
-    response = HTTParty.get("https://dictionaryapi.com/api/v3/references/thesaurus/json/#{word_query}?key=#{ENV['MW_thesaurus_api_key']}")
-    response['meta']
+  def self.look_up(word)
+    HTTParty.get("https://dictionaryapi.com/api/v3/references/thesaurus/json/#{word}?key=#{ENV['MW_thesaurus_api_key']}")[0]
   end
 end
