1209356df5591b857de5459e0b9055838361ef27
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-02 19:44:48 -0600

    add user create action with jwt auth

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,9 +1,20 @@
 class Api::UsersController < ApplicationController
-  before_action :authenticate_request!, except: [:create, :login] # Exclude this route from authentication
+  #before_action :authenticate_request!, except: [:create, :login] # Exclude this route from authentication
   #just for testing things out atm
   def index
     users = User.all
     render json: users
+  end
+
+  def create
+    @user = User.new(user_params)
+  
+    if @user.save && @user.authenticate(user_params[:password])
+      auth_token = JsonWebToken.encode(user_id: @user.id)
+      render json: { auth_token: auth_token }, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
   def login
@@ -15,5 +26,11 @@ class Api::UsersController < ApplicationController
     else
       render json: { error: 'Invalid username/password' }, status: :unauthorized
     end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
