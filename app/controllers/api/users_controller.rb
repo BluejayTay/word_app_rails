085aa8a3230e7fb2b630189239b0a1c24500c0f8
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-04 14:49:02 -0600

    fix create and login actions for users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,24 +1,19 @@
 class Api::UsersController < ApplicationController
-  #before_action :authenticate_request!, except: [:create, :login] # Exclude this route from authentication
-  #just for testing things out atm
-  def index
-    users = User.all
-    render json: users
-  end
-
-  def create
-    @user = User.new(user_params)
+  #before_action :authenticate_request!, except: [:create, :login]
   
-    if @user.save && @user.authenticate(user_params[:password])
-      auth_token = JsonWebToken.encode(user_id: @user.id)
+  def create
+    user = User.new(user_params)
+  
+    if user.save && user.authenticate(params[:password])
+      auth_token = JsonWebToken.encode(user_id: user.id)
       render json: { auth_token: auth_token }, status: :ok
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: user.errors, status: :unprocessable_entity
     end
   end
 
   def login
-    user = User.find_by(email: user_params[:email].to_s.downcase)
+    user = User.find_by(email: params[:email])
 
     if user&.authenticate(user_params[:password])
       auth_token = JsonWebToken.encode(user_id: user.id)
@@ -31,6 +26,6 @@ class Api::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.permit(:email, :password)
   end
 end
