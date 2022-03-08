f02a52cb699961a2b88a86aefc859081a6468442
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-08 14:51:10 -0600

    Update words_controller.rb
    codes index, private methods

--- app/controllers/api/words_controller.rb
+++ app/controllers/api/words_controller.rb
@@ -1,2 +1,20 @@
 class Api::WordsController < ApplicationController
+  
+  def index # GET api/study_lists/:study_list_id/words'
+    render json: {
+      study_list: study_list,
+      words: words
+    }
+  end
+
+  private
+
+  def study_list
+    @study_list ||= StudyList.find(params[:study_list_id])
+  end
+
+  def words
+    study_list.words
+  end
+
 end
