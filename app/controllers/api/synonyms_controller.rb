a0dc09bfb0df361a993bfb03a019512179d0cc2f
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-08 14:51:24 -0600

    Update synonyms_controller.rb
    codes index, private methods

--- app/controllers/api/synonyms_controller.rb
+++ app/controllers/api/synonyms_controller.rb
@@ -1,2 +1,19 @@
 class Api::SynonymsController < ApplicationController
+
+  def index # GET api/words/:word_id/synonyms
+    render json: {
+      word: word,
+      synonyms: synonyms
+    }
+  end
+  
+  private
+  def word
+    Word.find(params[:word_id])
+  end
+
+  def synonyms
+    word.synonyms
+  end
+
 end
