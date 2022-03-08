6324adcfa9560e73d6026e6e523fd0259a61dedb
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-08 16:18:56 -0600

    Update thesaurus_service.rb
    change argument to word_query

--- app/services/thesaurus_service.rb
+++ app/services/thesaurus_service.rb
@@ -1,5 +1,5 @@
 class ThesaurusService
-  def self.look_up(word)
-    HTTParty.get("https://dictionaryapi.com/api/v3/references/thesaurus/json/#{word}?key=#{ENV['MW_thesaurus_api_key']}")[0]
+  def self.look_up(word_query)
+    HTTParty.get("https://dictionaryapi.com/api/v3/references/thesaurus/json/#{word_query}?key=#{ENV['MW_thesaurus_api_key']}")[0]
   end
 end
