84be866a348217015f9cb93362290a51ffe41e81
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-09 17:31:42 -0600

    Add name uniqueness validation to word, synonym models

--- app/models/synonym.rb
+++ app/models/synonym.rb
@@ -1,3 +1,4 @@
 class Synonym < ApplicationRecord
   has_and_belongs_to_many :words
+  validates_uniqueness_of :name
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,5 @@
 class Word < ApplicationRecord
   has_and_belongs_to_many :synonyms
   has_and_belongs_to_many :study_lists
+  validates_uniqueness_of :name
 end
