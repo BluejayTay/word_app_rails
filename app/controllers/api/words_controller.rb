3fa469a5a4e07242f9e8a2ac6b12db8a13572554
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-09 16:34:02 -0600

    Update words_controller.rb
    code words#search

--- app/controllers/api/words_controller.rb
+++ app/controllers/api/words_controller.rb
@@ -1,13 +1,28 @@
 class Api::WordsController < ApplicationController
   
+  def search #GET api/words/search
+    word_query = params[:name]
+
+    if word_query.present?
+      @word = ThesaurusService.look_up(word_query)
+      
+      render json: {
+        name: @word['meta']['id'],
+        definition: @word['shortdef'],
+        synonyms: @word['meta']['syns'][0]
+      }
+    end
+  end
+  
   def index # GET api/study_lists/:study_list_id/words'
     render json: {
       study_list: study_list,
       words: words
     }
   end
-
+  
   private
+
 
   def study_list
     @study_list ||= StudyList.find(params[:study_list_id])
