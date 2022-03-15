0eeeb938527b5614d71df2c7d2ad072516b09415
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-15 18:50:41 -0500

    change new_game route to use get action

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,12 @@ Rails.application.routes.draw do
       end
     end
     resources :study_lists
-    post 'study_lists/:id/new_game', to: 'study_lists#new_game'
+    get 'study_lists/:id/new_game', to: 'study_lists#new_game'
     get 'study_lists/:study_list_id/words', to: 'words#index'
+    get 'words/:word_id/synonyms', to: 'synonyms#index'
     post 'study_lists/:id/words', to: 'study_lists#add_words'
     get 'words/search', to: 'words#search'
-    get 'words/:word_id/synonyms', to: 'synonyms#index'
+    
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
