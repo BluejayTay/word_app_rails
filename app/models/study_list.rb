475f1efefa3d830d63dc1bc45475421ead0501d5
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-25 15:50:38 -0500

    adds custom word_count validation to StudyList

--- app/models/study_list.rb
+++ app/models/study_list.rb
@@ -1,5 +1,17 @@
 class StudyList < ApplicationRecord
   belongs_to :user, optional: true
   has_and_belongs_to_many :words
-  validates :title, presence: true
+  validates :title, presence: true, length: {minimum: 1, maximum: 50}
+  validate :valid_word_count
+  
+  def word_count
+    self.words.count
+  end
+
+  def valid_word_count
+    if self.words.count > 10
+      errors.add(:word_count, "Must be less than or equal to 10")
+    end
+  end
+
 end
