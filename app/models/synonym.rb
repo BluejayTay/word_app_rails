f6f6e1b18156c34b0ca5552590cbfd35fabd8dc9
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-31 19:44:40 -0500

    adds more validations to word, synonym

--- app/models/synonym.rb
+++ app/models/synonym.rb
@@ -1,4 +1,5 @@
 class Synonym < ApplicationRecord
   has_and_belongs_to_many :words
   validates_uniqueness_of :name
+  validates :name, presence: true
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -2,4 +2,6 @@ class Word < ApplicationRecord
   has_and_belongs_to_many :synonyms
   has_and_belongs_to_many :study_lists
   validates_uniqueness_of :name
+  validates :definition, presence: true
+  validates :name, presence: true
 end
