4fd1ca3f865d0f0ebd0ef95043d9930ff0407ed9
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-31 19:39:14 -0500

    comments out entire words controller

--- app/controllers/api/words_controller.rb
+++ app/controllers/api/words_controller.rb
@@ -1,35 +1,25 @@
 class Api::WordsController < ApplicationController
   
-  def search #GET api/words/search
-    word_query = params[:name]
+  # def search #GET api/words/search
+  #   word_query = params[:name]
 
-    if word_query.present?
-      @word = ThesaurusService.look_up(word_query)
+  #   if word_query.present?
+  #     @word = ThesaurusService.look_up(word_query)
       
-      render json: {
-        name: @word['meta']['id'],
-        definition: @word['shortdef'],
-        synonyms: @word['meta']['syns'][0]
-      }
-    end
-  end
-  
-  def index # GET api/study_lists/:study_list_id/words'
-    render json: {
-      study_list: study_list,
-      words: words
-    }
-  end
-  
-  private
+  #     render json: {
+  #       name: @word['meta']['id'],
+  #       definition: @word['shortdef'],
+  #       synonyms: @word['meta']['syns'][0]
+  #     }
+  #   end
+  # end
 
+  # def show
+  #   @word = Word.find(id: params["id"])
 
-  def study_list
-    @study_list ||= StudyList.find(params[:study_list_id])
-  end
-
-  def words
-    study_list.words
-  end
+  #   render json: {
+  #     word: WordSerializer.new(word),
+  #     synonyms: @word.synonyms }
+  # end 
 
 end
