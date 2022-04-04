ddda7e2e529350d1052d02e073a84bed3acb4f93
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-04-04 18:46:03 -0500

    adds password confirmation to create user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -32,6 +32,6 @@ class Api::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
