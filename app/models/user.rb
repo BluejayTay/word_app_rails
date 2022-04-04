97a6dedbb04119eed372a113151a0a7490c57a62
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-04-04 18:46:59 -0500

    updates user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :study_lists
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, allow_blank: false
 end
