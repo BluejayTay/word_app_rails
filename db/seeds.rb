0f74ca988a17d2f0993fb771289966b8b4baccb9
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-04-06 11:17:52 -0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,6 @@ Word.destroy_all
 Synonym.destroy_all
 
 StudyList.create!([
-  {study_list: {title: "Beginner Blurbs"}, words: ["Hot", "Cold", "Big", "Small", "Happy", "Sad", "Near", "Far", "Empty", "Full"]},
-  {study_list: {title: "Crossword Commodities"}, words: ["Ante", "Bane", "Cabal", "Dyad", "Haft", "Icon", "Iota", "Meld", "Onus", "Skew"]},
-  {study_list: {title: "Ivy League Locutions"}, words: ["Abate", "Abjure", "Benevolent", "Deplore", "Ephemeral", "Gregarious", "Lilliputian", "Perfidious", "Rancorous", "Venerable"]},])
+  {{study_list: {title: "Beginner Blurbs"}}, {words: ["Hot", "Cold", "Big", "Small", "Happy", "Sad", "Near", "Far", "Empty", "Full"]}},
+  {{study_list: {title: "Crossword Commodities"}}, {words: ["Ante", "Bane", "Cabal", "Dyad", "Haft", "Icon", "Iota", "Meld", "Onus", "Skew"]}},
+  {{study_list: {title: "Ivy League Locutions"}}, {words: ["Abate", "Abjure", "Benevolent", "Deplore", "Ephemeral", "Gregarious", "Lilliputian", "Perfidious", "Rancorous", "Venerable"]}},])
