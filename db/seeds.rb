2f7717d078e1f8f73f89676b3b0d000e8833d38d
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-04-06 13:38:18 -0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,18 +9,18 @@
 #Word.destroy_all
 #Synonym.destroy_all
 
-# beginner_list = StudyList.create({title: "Beginner Blurbs"}) 
-# beginner_words = ["Hot", "Cold", "Big", "Small", "Happy", "Sad", "Near", "Far", "Empty", "Full"]
-# beginner_words.each do |word|
-#   result = ThesaurusService.look_up(word)
-#   new_word = Word.create!(name: result['meta']['id'], definition: result['shortdef'].join("; ").to_s)
-#   beginner_list.words << new_word
-#   synonyms_result = result['meta']['syns'][0]
-#   synonyms_result.each do |synonym|
-#     new_or_databased_synonym = Synonym.find_or_create_by!(name: synonym)
-#     new_word.synonyms << new_or_databased_synonym
-#     end
-#   end 
+beginner_list = StudyList.create({title: "Beginner Blurbs"}) 
+beginner_words = ["Hot", "Cold", "Big", "Small", "Happy", "Sad", "Near", "Far", "Empty", "Full"]
+beginner_words.each do |word|
+  result = ThesaurusService.look_up(word)
+  new_word = Word.create!(name: result['meta']['id'], definition: result['shortdef'].join("; ").to_s)
+  beginner_list.words << new_word
+  synonyms_result = result['meta']['syns'][0]
+  synonyms_result.each do |synonym|
+    new_or_databased_synonym = Synonym.find_or_create_by!(name: synonym)
+    new_word.synonyms << new_or_databased_synonym
+    end
+  end 
 
 crossword_list = StudyList.create({title: "Crossword Commodities"}) 
 crossword_words = ["Ante", "Bane", "Cabal", "Dyad", "Haft", "Icon", "Iota", "Meld", "Onus", "Skew"]
