bd24e6eb0ac2ea68cc33b4280f610fe77e176547
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-04-06 11:03:27 -0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,63 +9,12 @@ StudyList.destroy_all
 Word.destroy_all
 Synonym.destroy_all
 
-StudyList.create!([{
+StudyList.create!([{{
   title: "Beginner Bitties"
-},
-{
+}, words: ["Hot", "Cold", "Big", "Small", "Happy", "Sad", "Near", "Far", "Empty", "Full"]},
+{{
   title: "Crossword Commodities"
-},
-{
+}, words: ["Ante", "Bane", "Cabal", "Dyad", "Haft", "Icon", "Iota", "Meld", "Onus", "Skew"]},
+{{
   title: "Ivy League Locutions"
-}])
-
-Beginner_words = ["Hot", "Cold", "Big", "Small", "Happy", "Sad", "Near", "Far", "Empty", "Full"]
-Beginner_words.each do |word|
-  result = ThesaurusService.look_up(word)
-  created_word = Word.create!(name: result['meta']['id'], definition: result['shortdef'].join("; ").to_s)
-  StudyList.first.words << created_word
-  synonyms = result['meta']['syns'][0]
-  synonyms.each do |synonym|
-    if Synonym.find_by(name: synonym)
-      synonym_to_add = Synonym.find_by(name: synonym.to_s)
-      created_word.synonyms << synonym_to_add
-    else
-      created_synonym = Synonym.create!(name: synonym.to_s)
-      created_word.synonyms << created_synonym
-    end
-  end
-end
-
-Crossword_words = ["Ante", "Bane", "Cabal", "Dyad", "Haft", "Icon", "Iota", "Meld", "Onus", "Skew"]
-Crossword_words.each do |word|
-  result = ThesaurusService.look_up(word)
-  created_word = Word.create!(name: result['meta']['id'], definition: result['shortdef'].join("; ").to_s)
-  StudyList.second.words << created_word
-  synonyms = result['meta']['syns'][0]
-  synonyms.each do |synonym|
-    if Synonym.find_by(name: synonym)
-      synonym_to_add = Synonym.find_by(name: synonym.to_s)
-      created_word.synonyms << synonym_to_add
-    else
-      created_synonym = Synonym.create!(name: synonym.to_s)
-      created_word.synonyms << created_synonym
-    end
-  end
-end
-
-Ivy_words = ["Abate", "Abjure", "Benevolent", "Deplore", "Ephemeral", "Gregarious", "Lilliputian", "Perfidious", "Rancorous", "Venerable"]
-Ivy_words.each do |word|
-  result = ThesaurusService.look_up(word)
-  created_word = Word.create!(name: result['meta']['id'], definition: result['shortdef'].join("; ").to_s)
-  StudyList.third.words << created_word
-  synonyms = result['meta']['syns'][0]
-  synonyms.each do |synonym|
-    if Synonym.find_by(name: synonym)
-      synonym_to_add = Synonym.find_by(name: synonym.to_s)
-      created_word.synonyms << synonym_to_add
-    else
-      created_synonym = Synonym.create!(name: synonym.to_s)
-      created_word.synonyms << created_synonym
-    end
-  end
-end
+}, words: ["Abate", "Abjure", "Benevolent", "Deplore", "Ephemeral", "Gregarious", "Lilliputian", "Perfidious", "Rancorous", "Venerable"]}])
