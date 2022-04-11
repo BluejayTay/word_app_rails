9680ebf84e7f1b035a011fb87b52202607796c5c
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-04-11 10:56:42 -0500

    refactors error handling for thesaurus object

--- app/services/thesaurus_service.rb
+++ app/services/thesaurus_service.rb
@@ -1,7 +1,8 @@
 class ThesaurusService
   def self.look_up(word_query)
     result = HTTParty.get("https://dictionaryapi.com/api/v3/references/thesaurus/json/#{word_query}?key=#{ENV['MW_thesaurus_api_key']}")[0]
-    if result.nil? || result.is_a?(String)
+
+    if result.nil? || !result.is_a?(Hash) 
       return "Error: Word not found"
     else
       return result
