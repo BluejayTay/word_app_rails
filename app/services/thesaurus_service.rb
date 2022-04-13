f927fc264e1d548a3b4cded57f67f9d21b509fff
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-04-13 16:15:08 -0500

    fixes thesaurus service from rubocop changes

--- app/services/thesaurus_service.rb
+++ app/services/thesaurus_service.rb
@@ -2,12 +2,11 @@
 
 class ThesaurusService
   def self.look_up(word_query)
-    result = HTTParty.get("https://dictionaryapi.com/api/v3/references/thesaurus/json/#{word_query}?key=#{ENV['MW_thesaurus_api_key']}")[0]
-
-    if result.nil? || !result.is_a?(Hash)
-      'Error: Word not found'
+    response = HTTParty.get("https://dictionaryapi.com/api/v3/references/thesaurus/json/#{word_query}?key=#{ENV['MW_thesaurus_api_key']}")[0]
+    if response.nil? || response.is_a?(String)
+      return 'Error: Word not found'
     else
-      result
+      return response
     end
   end
 end
