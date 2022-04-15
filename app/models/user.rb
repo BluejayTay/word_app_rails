ab525bd8205d0e55357c97c2d0c1b0e309daa924
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-04-15 12:49:55 -0500

    adds method to downcase email before validating

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ApplicationRecord
 
   has_many :study_lists
   validates :email, presence: true, uniqueness: true
+  before_validation :normalize_email
   validates :password_digest, presence: true
   validates :password, length: { in: 6..50 }, on: :create
+
+  def normalize_email
+    self.email = self.email.downcase.strip
+  end
+
 end
